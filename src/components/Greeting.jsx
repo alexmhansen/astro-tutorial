0b71ab75a554386d11f5f333d9d620eba5aed489
Alexander Hansen <12524054+alexmhansen@users.noreply.github.com>
2023-07-03 16:39:53 -0700

    Astro island (#1)
    * Added cutom greeting on Home Page
    
    * Night mode
    
    ---------
    
    Co-authored-by: Alexander Hansen <alex@Alexanders-MacBook-Air.local>

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,19 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const [greeting, setgreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setgreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  );
+
+}
